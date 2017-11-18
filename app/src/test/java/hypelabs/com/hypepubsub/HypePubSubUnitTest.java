891a23cfa888754dc685e52473dd5ea641b7fb5e
Xavier Araújo <xa@hypelabs.io>
2017-11-18 17:35:14 +0000

    Added file to write unit tests for the HypePubSub class

---
+++ app/src/test/java/hypelabs/com/hypepubsub/HypePubSubUnitTest.java
@@ -0,0 +1,14 @@
+package hypelabs.com.hypepubsub;
+
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
+public class HypePubSubUnitTest
+{
+    
+}
