101cc6f9675d665487f064daa7b85025dc922c9e
Xavier Araújo <xa@hypelabs.io>
2018-10-26 08:12:22 +0100

    Added gitignore file. Removed test app identifier

--- app/src/main/java/hypelabs/com/hypepubsub/HpsConstants.java
+++ app/src/main/java/hypelabs/com/hypepubsub/HpsConstants.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 
 public class HpsConstants
 {
-    static public final String APP_IDENTIFIER = "ef412c4d";
+    static public final String APP_IDENTIFIER = "{{app_identifier))";
     static public final String HASH_ALGORITHM = "SHA-1";
     static public final int HASH_ALGORITHM_DIGEST_LENGTH = 20;
     static public final String ENCODING_STANDARD = "UTF-8";
